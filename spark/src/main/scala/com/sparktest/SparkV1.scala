80819b6950578ca57b087720e0ea4768c61d3486
dzzxjl <dzzxjl@126.com>
2023-03-09 23:44:44 +0800

    [feat] add SparkV2

--- spark/src/main/scala/com/sparktest/SparkV1.scala
+++ spark/src/main/scala/com/sparktest/SparkV1.scala
@@ -1,5 +1,6 @@
 package com.sparktest
 
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
@@ -8,12 +9,17 @@ import org.apache.spark.{SparkConf, SparkContext}
 object SparkV1 {
 
 	def main(args: Array[String]) {
-		val conf = new SparkConf().setAppName("mySpark")
-		//setMaster("local") 本机的spark就用local，远端的就写ip
-		//如果是打成jar包运行则需要去掉 setMaster("local")因为在参数中会指定。
-		conf.setMaster("local")
+
+		// 本机的spark就用local，远端的就写ip
+		// 如果是打成jar包运行则需要去掉 setMaster("local")因为在参数中会指定。
+
+		val conf = new SparkConf()
+			.setAppName("mySpark")
+			.setMaster("local")
+
 		val sc = new SparkContext(conf)
-		//    val rdd = sc.parallelize(List(1,2,3,4,5,6)).map(_*3)
+
+		// val rdd = sc.parallelize(List(1,2,3,4,5,6)).map(_*3)
 		val rdd = sc.parallelize(List(1, 2, 3, 4, 5, 6))
 		// val rdd = sc.parallelize(List(1, 2, 3, 4, 5, 6))
 
